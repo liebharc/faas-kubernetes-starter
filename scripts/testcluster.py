1ecea5cdbca0cd3f38c1c9ec6cba436db3625b5e
Christian Liebhardt <christian.liebhardt@arcor.de>
2022-02-23 17:02:07 +0100

    Fixed package.json

---
+++ scripts/testcluster.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import re
+import requests
+
+def capture_std_out_of_command(command, numberOfLines = -1):
+    import subprocess
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    capture = ""
+
+    s = process.stdout.read(1)
+    while len(s) > 0:
+        sys.stdout.write(s)
+        sys.stdout.flush()
+        capture += s
+        if numberOfLines > 0 and len(capture.splitlines()) >= numberOfLines:
+            break
+        s = process.stdout.read(1)
+
+    return capture
+
+def open_tunnel_and_get_url():
+    stdout = capture_std_out_of_command("minikube service ingress-nginx-controller --namespace ingress-nginx --url", numberOfLines=9)
+    is_host = re.compile("(http://(.*):(.*))")
+    for line in stdout.splitlines():
+        match = is_host.match(line)
+        if match:
+            return match.group(1)
+    return None
+
+
+url = open_tunnel_and_get_url()
+hello_url = url + "/v1/hello"
+print("Hello URL: " + hello_url)
+print(requests.get(hello_url).text)
